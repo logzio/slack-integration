c4a8423471240075ac01ba7657568f7687b12ec6
Asaf Alima <asaf.alima@gmail.com>
2018-05-22 12:12:48 +0300

    Export jasmine config to json file

--- jasmine.js
+++ jasmine.js
@@ -2,17 +2,7 @@ const Jasmine = require('jasmine');
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 const jasmine = new Jasmine();
-jasmine.loadConfig({
-  "spec_dir": "src",
-  "spec_files": [
-    "**/*[sS]pec.js"
-  ],
-  "helpers": [
-    "helpers/**/*.js"
-  ],
-  stopSpecOnExpectationFailure: false,
-  random: false
-});
+jasmine.loadConfigFile('jasmine.json');
 
 jasmine.clearReporters();
 jasmine.addReporter(new SpecReporter());
