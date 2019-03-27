824a0328dc4407b3e7fc3527b6754eb2f68729d7
irinapundik <irina.p@logz.io>
2019-03-27 12:37:05 +0200

    tests

--- __tests__/get-channel-account.spec.js
+++ __tests__/get-channel-account.spec.js
@@ -87,26 +87,25 @@ describe('Get channel account', () => {
         expect(message.text).toBe(Messages.getCurrentChannel(alias2));
         done();
       })
-
-      // .then(() =>
-      //   globalTestConfiguration.bot.usersInput(
-      //     TestFunctions.removeAccount(userId, teamId, alias2, channelId)
-      //   )
-      // )
-      // .then(() => {
-      //   globalTestConfiguration.bot
-      //     .usersInput(
-      //       TestFunctions.confirm(userId, teamId, alias2, channelId, 'remove-yes')
-      //     )
-      //     .then(message => {
-      //       expect(message.text).toBe(`Okay, I removed ${alias2} from Slack.`);
-      //       globalTestConfiguration.bot.usersInput(TestFunctions.getChannelAccount(userId, teamId, channelId2))
-      //         .then(message => {
-      //           expect(message.text).toBe(Messages.NO_CHANNEL_ACCOUNT);
-      //           done();
-      //         });
-      //     });
-      // });
+      .then(() =>
+        globalTestConfiguration.bot.usersInput(
+          TestFunctions.removeAccount(userId, teamId, alias2, channelId)
+        )
+      )
+      .then(() => {
+        globalTestConfiguration.bot
+          .usersInput(
+            TestFunctions.confirm(userId, teamId, alias2, channelId, 'remove-yes')
+          )
+          .then(message => {
+            expect(message.text).toBe(`Okay, I removed ${alias2} from Slack.`);
+            globalTestConfiguration.bot.usersInput(TestFunctions.getChannelAccount(userId, teamId, channelId2))
+              .then(message => {
+                expect(message.text).toBe(Messages.NO_CHANNEL_ACCOUNT);
+                done();
+              });
+          });
+      });
   });
 
   beforeAll(async done => {
