fc482c68d1940a35742346e22da4363b1c4d11c2
irinapundik <irina.p@logz.io>
2019-04-01 12:18:57 +0300

    flaky

--- __tests__/mixed-defaults.spec.js
+++ __tests__/mixed-defaults.spec.js
@@ -1,6 +1,6 @@
 const GlobalConfiguration = require('../src/core/utils/globalTestConfigurationSetup');
-const CommandName = require('../__tests__/commandName');
-const TestFunctions = require('../__tests__/testFunctions');
+const CommandName = require('./commandName');
+const TestFunctions = require('./testFunctions');
 const Messages = require('../src/core/messages/messages');
 const userId = 'u_mixed1';
 const teamId = 't_mixed1';
@@ -455,10 +455,10 @@ describe('Mixed1', () => {
     done();
   });
 
-  afterAll(done => {
+  afterAll(async done => {
     globalTestConfiguration.afterAll(done);
   });
-  afterEach((done) => {
+  afterEach(async(done) => {
     globalTestConfiguration.afterEach(done);
   });
 });
