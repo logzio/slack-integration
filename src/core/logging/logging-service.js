ed35e18228b5a685e15b75094dc6bc2f91a83f8a
TzurZ <tzur.ziv@logz.io>
2021-02-14 21:53:18 +0200

    Fix userObject->message on one end

--- src/core/logging/logging-service.js
+++ src/core/logging/logging-service.js
@@ -1,13 +1,13 @@
 const { getEventMetadata } = require('./logging-metadata');
 
 const logEvent = ({ userObject, action, eventName, logger, companyName }) => {
-  logger.info(
-    `User ${userObject.user} from team ${userObject.team}, customer name ${companyName} ${action}`,
-    getEventMetadata({ userObject, eventName, companyName })
-  );
-
   console.log(
     `User ${userObject.user} from team ${userObject.team}, customer name ${companyName} ${action}`
+  );
+
+  logger.info(
+    `User ${userObject.user} from team ${userObject.team}, customer name ${companyName} ${action}`,
+    getEventMetadata({ message: userObject, eventName, companyName })
   );
 };
 
